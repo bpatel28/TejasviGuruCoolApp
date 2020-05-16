7db2aa332d069a806ee6c4658e6d1e15d78e1309
Brijesh Patel <b.patel9528@gmail.com>
2020-05-16 17:24:25 -0600

    showing user information in app drawer

--- tejasvi_gurucool/lib/main.dart
+++ tejasvi_gurucool/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:tejasvi_gurucool/screens/login_screen.dart';
 import 'package:tejasvi_gurucool/screens/register_screen.dart';
+import 'package:tejasvi_gurucool/models/user_model.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,6 +11,18 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    final User user = new User(
+      id: -1,
+      firstName: "Brijesh",
+      lastName: "Patel",
+      middleName: "Jayendrabhai",
+      birthDate: new DateTime(1995, 5, 28),
+      phoneNo: 5877001874,
+      email: "b.patel9528@gmail.com",
+      password: "1234567",
+      createdOn: DateTime.now().subtract(new Duration(days: 5)),
+      updatedOn: DateTime.now(),
+    );
     return MaterialApp(
       title: 'Tejasvi GuruCool',
       debugShowCheckedModeBanner: false,
@@ -24,26 +37,46 @@ class MyApp extends StatelessWidget {
         '/login': (context) => LoginScreen(),
         '/register': (context) => RegisterScreen(),
       },
-      home: MyHomePage(),
+      home: MyHomePage(UniqueKey(), user),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
+  final User _user;
+
+  MyHomePage(Key key, this._user) : super(key: key);
+
   Widget _createHeader(BuildContext context) {
     return DrawerHeader(
         margin: EdgeInsets.zero,
         padding: EdgeInsets.only(top: 15.0, left: 10.0),
         decoration: BoxDecoration(color: Theme.of(context).primaryColor),
-        child: Text(
-          "Home",
-          style: TextStyle(
-              color: Colors.white, fontSize: 20.0, fontWeight: FontWeight.w500),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              _user.getName(),
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 22.0,
+                  fontWeight: FontWeight.w500),
+            ),
+            Text(
+              "Batch 10A",
+              style: TextStyle(
+                color: Colors.black,
+              ),
+            )
+          ],
         ));
   }
 
   Widget _createDrawerItem(BuildContext context,
-      {IconData icon, String text, GestureTapCallback onTap}) {
+      {IconData icon,
+      String text,
+      GestureTapCallback onTap,
+      bool selected: false}) {
     return ListTile(
       title: Row(
         children: <Widget>[
@@ -54,6 +87,7 @@ class MyHomePage extends StatelessWidget {
           )
         ],
       ),
+      selected: selected,
       onTap: onTap,
     );
   }
@@ -67,7 +101,7 @@ class MyHomePage extends StatelessWidget {
   }
 
   void _onMyProfileTap(BuildContext context) {
-    Navigator.pop(context); 
+    Navigator.pop(context);
   }
 
   void _onAboutUsTap(BuildContext context) {
@@ -89,13 +123,22 @@ class MyHomePage extends StatelessWidget {
           children: <Widget>[
             _createHeader(context),
             _createDrawerItem(context,
-                icon: Icons.subject, text: "Subjects", onTap: () => _onSubjectsTap(context)),
+                icon: Icons.subject,
+                text: "Subjects",
+                onTap: () => _onSubjectsTap(context),
+                selected: true),
             _createDrawerItem(context,
-                icon: Icons.announcement, text: "Announcements", onTap: () => _onAnnouncementsTap(context)),
+                icon: Icons.announcement,
+                text: "Announcements",
+                onTap: () => _onAnnouncementsTap(context)),
             _createDrawerItem(context,
-                icon: Icons.people, text: "My Profile", onTap: () => _onMyProfileTap(context)),
+                icon: Icons.people,
+                text: "My Profile",
+                onTap: () => _onMyProfileTap(context)),
             _createDrawerItem(context,
-                icon: Icons.info, text: "About Us", onTap: () => _onAboutUsTap(context))
+                icon: Icons.info,
+                text: "About Us",
+                onTap: () => _onAboutUsTap(context))
           ],
         ),
       ),
---
+++ tejasvi_gurucool/lib/models/user_model.dart
@@ -0,0 +1,94 @@
+class User {
+  int _id;
+  int get id => _id;
+
+  String _firstName;
+  String get firstName => _firstName;
+
+  String _lastName;
+  String get lastName => _lastName;
+
+  String _middleName;
+  String get middleName => _middleName;
+
+  DateTime _birthDate;
+  DateTime get birthDate => _birthDate;
+
+  int _phoneNo;
+  int get phoneNo => _phoneNo;
+
+  int _username;
+  int get userName => _username;
+
+  String _email;
+  String get email => _email;
+
+  String _password;
+  String get password => _password;
+
+  DateTime _createdOn;
+  DateTime get createdOn => _createdOn;
+
+  DateTime _updatedOn;
+  DateTime get updatedOn => _updatedOn;
+
+  String getInitials() {
+    try {
+      return _firstName[0].toUpperCase() + _lastName[0].toUpperCase();
+    } on Exception catch (e) {
+      print(e.toString());
+      return "";
+    }
+  }
+
+  String getName() {
+    try {
+      return _firstName[0].toUpperCase() +
+          _firstName.substring(1) +
+          " " +
+          _lastName[0].toUpperCase() +
+          _lastName.substring(1);
+    } on Exception catch (e) {
+      print(e.toString());
+      return "";
+    }
+  }
+
+  String getFullName() {
+    try {
+      return _firstName[0].toUpperCase() +
+          _firstName.substring(1) +
+          " " +
+          (_middleName.length > 0
+              ? _middleName[0].toUpperCase() + _middleName.substring(1) + " "
+              : "") +
+          _lastName[0].toUpperCase() +
+          _lastName.substring(1);
+    } on Exception catch (e) {
+      print(e.toString());
+      return "";
+    }
+  }
+
+  User(
+      {int id,
+      String firstName,
+      String lastName,
+      String middleName,
+      DateTime birthDate,
+      int phoneNo,
+      String email,
+      String password,
+      DateTime createdOn,
+      DateTime updatedOn})
+      : _id = id,
+        _firstName = firstName,
+        _lastName = lastName,
+        _middleName = middleName,
+        _birthDate = birthDate,
+        _phoneNo = phoneNo,
+        _email = email,
+        _password = password,
+        _createdOn = createdOn,
+        _updatedOn = updatedOn;
+}
