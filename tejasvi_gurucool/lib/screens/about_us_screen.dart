1110546dda174f903fe2be4f585e92b8bb7275ac
Brijesh Patel <b.patel9528@gmail.com>
2020-05-24 14:48:04 -0600

    added content for about us page.

--- tejasvi_gurucool/lib/screens/about_us_screen.dart
+++ tejasvi_gurucool/lib/screens/about_us_screen.dart
@@ -15,8 +15,25 @@ class AboutUsScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text("About Us"),
       ),
-      body: Center(
-        child: Text("About Page"),
+      body: ListView(
+        children: <Widget>[
+          Icon(Icons.school, size: 200),
+          Text(
+            "Tejasvi GuruCool",
+            style: TextStyle(
+              fontSize: 18.0,
+              fontWeight: FontWeight.bold,
+            ),
+            textAlign: TextAlign.center,
+          ),
+          Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Text(
+              "Tejasvi gurucool is Gujarat's premier coaching centre which continuously innovates to help students actualize their dreams through integrated efforts of an interactive learning system.",
+              style: TextStyle(fontSize: 14.0),
+            ),
+          )
+        ],
       ),
       drawer: AppDrawer(_user, Routes.ABOUT_US),
     );
